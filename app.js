b3d6f67e087a946eaee74c673d4abf4c7574df0c
KaurTamm <kaur.tamm@khk.ee>
2019-11-12 15:33:57 +0200

    Id selector & style change

--- app.js
+++ app.js
@@ -1,37 +1,13 @@
-// document
-let sisu;
-sisu = document;                    // Sisu HTMLis
-sisu = document.all;                // HTML tagid kasutamis järjekorras
-sisu = document.all[2];             // Konkreetne HTML tag
-sisu = document.all.length;         // Palju on HTML tage kasutusel
-sisu = document.head;               // Head sisu
-sisu = document.body;               // Body sisu
-sisu = document.doctype;            // Doctype
-sisu = document.domain;             // Domeen
-sisu = document.URL;                // URL
-sisu = document.charset;            // Character set
-sisu = document.contentType;        // Sisu tüüp
+// document methods
+// single element selector
+sisu = document.getElementById('title');             // HTML sisu id järgi
+id = document.getElementById('title').id;            // id
+klass = document.getElementById('title').className;  // id klassid
 
-// form
-sisu = document.forms;              // Kõik formid
-sisu = document.forms[0];           // Esimene form
-sisu = document.forms[0].id;        // Esimese formi id
-sisu = document.forms[0].method;    // Form method
-sisu = document.forms[0].action;    // Form action
-
-// links
-sisu = document.links;              // Kõik lingid
-sisu = document.links[0];           // Esimene link
-sisu = document.links[0].className; // Esimese lingi klassid
-sisu = document.links[0].classList; // Esimese lingi klasside list
-
-// images
-sisu = document.images;             // Kõik pildid
-
-// scripts
-sisu = document.scripts;            // Kõik scriptid
-sisu = document.scripts[0].getAttribute('src');
-sisu = document.scripts[1].getAttribute('src');
-sisu = document.scripts[2].getAttribute('src');
+// style change
+sisu.style.background = '#333';
+document.getElementById('title').style.background = '#333';
+sisu.style.color = '#fff';
+sisu.style.padding = '5px'
 
 console.log(sisu);
