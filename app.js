a8938a901b1783039f3c9e4a66e8264adff79868
KaurTamm <kaur.tamm@khk.ee>
2019-11-12 15:05:08 +0200

    Document

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+//document
+let sisu;
+sisu = document;                // Sisu HTMLis
+sisu = document.all;            // HTML tagid kasutamis järjekorras
+sisu = document.all[2];         // Konkreetne HTML tag
+sisu = document.all.length;     // Palju on HTML tage kasutusel
+sisu = document.head;           // Head sisu
+sisu = document.body;           // Body sisu
+sisu = document.doctype;        // Doctype
+sisu = document.domain;         // Domeen
+sisu = document.URL;            // URL
+sisu = document.charset;        // Character set
+sisu = document.contentType;    // Sisu tüüp
 
+
+console.log(sisu);
