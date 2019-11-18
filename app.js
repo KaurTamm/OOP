488b46ad5bf13868d8fee3a8f61a4bf7a7a3cfb3
KaurTamm <kaur.tamm@khk.ee>
2019-11-18 11:39:42 +0200

    Removing classes & attributes

--- app.js
+++ app.js
@@ -7,5 +7,19 @@ liElement[2].remove();
 const ulElement = document.querySelector('ul.collection');
 ulElement.removeChild(liElement[1]);
 
-console.log(liElement);
-console.log(ulElement);
+// removing classes & attributes
+const liElements = liElement[0];
+const link = liElements.children[0];
+
+let sisu = link.className;
+sisu = link.classList;
+link.classList.remove('secondary-content');
+link.classList.add('secondary-content');
+
+sisu = link.getAttribute('href');
+link.setAttribute('href', 'https://google.com');
+link.setAttribute('title', 'google');
+link.removeAttribute('title');
+
+sisu = link;
+console.log(sisu);
