214a6ce688e4231c75d36560f2699eb0491efd4e
KaurTamm <kaur.tamm@khk.ee>
2019-11-18 11:13:56 +0200

    Replacing elements

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 // document methods
-// creating elements
-const liElement = document.createElement('li');
-// add class
-liElement.className = 'collection-item';
-// add id
-liElement.id = 'new-item';
-// add attribute
-liElement.setAttribute('title', 'new element');
-// add text
-liElement.textContent = 'Uus element';
-// add link
-const link = document.createElement('a');
-// add class
-link.className = 'delete-item secondary-content'
-// add attribute
-link.setAttribute('href', '#');
-// add icon
-link.innerHTML = '<i class="fas fa-times-circle"></i>'
-// add link to li
-liElement.appendChild(link);
-// add to DOM
-ulElement = document.querySelector('ul');
-ulElement.appendChild(liElement);
+// replacing elements
 
-console.log(liElement);
+const oldTitle = document.querySelector('h5');
+const divCardAction = document.querySelector('.card-action');
 
+const newTitle = document.createElement('h3');
+newTitle.id = 'new-title';
+newTitle.textContent = 'Uued ülesanded';
+// newTitle.appendChild(document.createTextNode('Uued ülesanded'))
+
+divCardAction.replaceChild(newTitle, oldTitle);
+
+
+console.log(newTitle);
+
+/////////////////////////////////////////////////////////////////
+
+const divCardTitle = document.querySelector('.card-content');
+const oldCardTitle = document.querySelector('.card-title');
+
+const newCardTitle = document.createElement('h4');
+newCardTitle.id = 'title';
+newCardTitle.textContent = 'Ülesanded';
+
+divCardTitle.replaceChild(newCardTitle, oldCardTitle);
