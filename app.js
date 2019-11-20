27dfb88f2c589e904e80a4f1e6c55e7ff7d564c4
KaurTamm <kaur.tamm@khk.ee>
2019-11-20 11:26:58 +0200

    More Events

--- app.js
+++ app.js
@@ -1,32 +1,41 @@
 // events
 
+//document.querySelector('.card-title').addEventListener('click', function(e){
+//    console.log('card-title');
+//});
+
+//document.querySelector('.card-content').addEventListener('click', function(e){
+//    console.log('card-content');
+//});
+function delfunct(e){
+    console.log('kustutame');
+    if(e.target.parentElement.classList.contains('delete-item')){
+    e.target.parentElement.parentElement.remove()};
+}
+
+const delTask = document.querySelector('.delete-item');
+delTask.addEventListener('click', delfunct);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // function deleteAllBtn(e){
 //    document.querySelectorAll('li').forEach(el => el.remove()); 
 //};
 //const deleteAll = document.querySelector('.clear-tasks');
 //deleteAll.addEventListener('click', deleteAllBtn);;
-
-function syndmus(e){
-    console.log(`Sündmuse tüüp: ${e.type}`);
-    console.log(newTaskDesc.value);
-    text.innerText = newTaskDesc.value;   
-}
-
-const text = document.querySelector('h5');
-const card = document.querySelector('.card');
-const form = document.querySelector('form');
-const newTaskDesc = document.getElementById('task');
-newTaskDesc.value = '';
-
-// form send
-form.addEventListener('submit', syndmus);
-
-// keyboard
-// newTaskDesc.addEventListener('keydown', syndmus);
-// newTaskDesc.addEventListener('keyup', syndmus);
-// newTaskDesc.addEventListener('keypress', syndmus);
-// newTaskDesc.addEventListener('focus', syndmus);
-// newTaskDesc.addEventListener('blur', syndmus);
-// newTaskDesc.addEventListener('paste', syndmus); 
-// newTaskDesc.addEventListener('cut', syndmus);
-newTaskDesc.addEventListener('input', syndmus);
