1ca21443ea82d2151ef34dcb05f94a683d7775d9
KaurTamm <kaur.tamm@khk.ee>
2019-11-20 12:31:54 +0200

    Localstorage

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
-// events
+// localstorage
 
-//document.querySelector('.card-title').addEventListener('click', function(e){
-//    console.log('card-title');
-//});
+//localStorage.setItem('nimi', 'Kaur');
+//localStorage.setItem('perekonnanimi', 'Tamm');
+//localStorage.removeItem('test');
 
-//document.querySelector('.card-content').addEventListener('click', function(e){
-//    console.log('card-content');
-//});
-function delfunct(e){
-    console.log('kustutame');
-    if(e.target.parentElement.classList.contains('delete-item')){
-    e.target.parentElement.parentElement.remove()};
+//const eesNimi = localStorage.getItem('nimi');
+//const pereNimi = localStorage.getItem('perekonnanimi');
+
+//console.log(eesNimi);
+//console.log(pereNimi);
+
+//localStorage.clear();
+
+document.querySelector('form').addEventListener('submit', saveTask);
+
+function saveTask(e){
+    const newTask = document.getElementById('task').value;
+    let allTasks;
+
+    if(localStorage.getItem('tasks') === null){
+        allTasks = [];
+    }else{
+        allTasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    allTasks.push(newTask);
+    localStorage.setItem('tasks', JSON.stringify(allTasks));
+    e.preventDefault();
 }
 
-const delTask = document.querySelector('.delete-item');
-delTask.addEventListener('click', delfunct);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const allTasks = JSON.parse(localStorage.getItem('tasks'));
 
 
 
