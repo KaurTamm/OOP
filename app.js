9502b2e449f213b2dc514ab38cefb488eaee6232
KaurTamm <kaur.tamm@khk.ee>
2019-11-20 09:26:36 +0200

    Target

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 // events
 
 function deleteAllBtn(e){
-    console.log('vajutatud');
+    let sisu = e;
+    sisu = e.target;
+    sisu = e.target.id;
+    sisu = e.target.className;
+    sisu = e.target.classList;
+    sisu = e.type;
+    sisu = e.clientY;
+    sisu = e.clientX;
+    sisu = e.offsetY;
+    sisu = e.offsetX;
+
+    console.log(sisu);
+
+    document.querySelectorAll('li').forEach(el => el.remove()); 
 };
 
 const deleteAll = document.querySelector('.clear-tasks');
-
 deleteAll.addEventListener('click', deleteAllBtn);
