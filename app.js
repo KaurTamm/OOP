92a8f4d7e3c811a5b9bfbebb7f634e0c96412032
KaurTamm <kaur.tamm@khk.ee>
2019-11-25 13:49:17 +0200

    Class

--- app.js
+++ app.js
@@ -1,29 +1,12 @@
-// Constructor
+// Class
 
-function Isik(e, p){
-    this.ees = e;
-    this.pere = p;
-};
-
-// Object.prototype
-
-Isik.prototype.taisNimi = function(){
-    return `${this.ees} ${this.pere}`
-};
-
-function Klient(e, p, t, s){
-    Isik.call(this, e, p);
-    this.tel = t;
-    this.status = s;
-};
-
-Klient.prototype = Object.create(Isik.prototype);
-Klient.prototype.constructor = Klient;
-
-Klient.prototype.taisNimi = function(){
-    return `${this.ees} ${this.pere} - sinul on ${this.status} tase`
+class Isik{
+    // Constructor
+    constructor(e, p){
+        this.eesnimi = e;
+        this.perenimi = p;
+    };
 }
 
-const kaur = new Klient('Kaur', 'Tamm', '1234 5678', 'silver');
+const kaur = new Isik('Kaur', 'Tamm');
 console.log(kaur);
-console.log(kaur.taisNimi());
