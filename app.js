89b5e7e0814ed8fa0bd0f56da8b0124e9c8b9b4a
KaurTamm <kaur.tamm@khk.ee>
2019-11-25 14:12:47 +0200

    Using Classes

--- app.js
+++ app.js
@@ -2,11 +2,30 @@
 
 class Isik{
     // Constructor
-    constructor(e, p){
+    constructor(e, p, skp){
         this.eesnimi = e;
         this.perenimi = p;
+        this.synnikuupaev = new Date(skp);
+    };
+    // Tervitus
+    tervitus(){
+        return `Tere, ${this.eesnimi} ${this.perenimi}!`
+    };
+    // Vanuse arvutamine
+    vanus(){
+        let diff = Date.now() - this.synnikuupaev.getTime();
+        diff = new Date(diff);
+        diff = diff.getUTCFullYear();
+        return diff - 1970;
+    };
+    abielus(uusPereNimi){
+        this.perenimi = uusPereNimi;
+        return this.perenimi;
     };
 }
 
-const kaur = new Isik('Kaur', 'Tamm');
-console.log(kaur);
+const kaur = new Isik('Kaur', 'Tamm', '1995-02-19');
+
+console.log(kaur.vanus());
+kaur.abielus('Random');
+console.log(kaur.tervitus());
