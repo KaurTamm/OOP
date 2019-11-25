40e0bf2ce1f93fd78798a8aa35d7936bc84f15ed
KaurTamm <kaur.tamm@khk.ee>
2019-11-25 11:48:57 +0200

    Object Prototype

--- app.js
+++ app.js
@@ -4,20 +4,25 @@ function Isik(e, p, skp){
     this.ees = e;
     this.pere = p;
     this.synnikuupaev = new Date(skp);
-
-    this.taisNimi = function(){
-        return `${this.ees} ${this.pere}`
-    };
-
-    this.getAge = function(){
-        const diff = Date.now() - this.synnikuupaev.getTime();
-        const vanusDateKujul = new Date(diff);
-        const taisAasta = vanusDateKujul.getUTCFullYear();
-        const vanus = taisAasta - 1970;
-        return vanus;
-    };
 };
 
+// Object.prototype
+
+Isik.prototype.taisNimi = function(){
+    return `${this.ees} ${this.pere}`
+};
+
+Isik.prototype.getAge = function(){
+    const diff = Date.now() - this.synnikuupaev.getTime();
+    const vanusDateKujul = new Date(diff);
+    const taisAasta = vanusDateKujul.getUTCFullYear();
+    const vanus = taisAasta - 1970;
+    return vanus;
+};
+
+
 const kaur = new Isik('Kaur', 'Tamm', '1995-02-19');
+
 console.log(kaur);
+console.log(kaur.taisNimi());
 console.log(kaur.getAge());
