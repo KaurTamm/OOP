684c51d395096a5df898ddd8300ca643c4af6494
KaurTamm <kaur.tamm@khk.ee>
2019-12-09 15:34:22 +0200

    New Weather Structure

--- weatherapp.js
+++ weatherapp.js
@@ -1,6 +1,6 @@
 function weatherBalloon( cityID ) {
     var key = 'f19190e7226af07a2dfe8b42df98b83a';
-    fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)  
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityID+ '&appid=' + key)  
     .then(function(resp) { return resp.json() }) // Convert data to json
     .then(function(data) {
         console.log(data);
@@ -29,5 +29,5 @@ function drawWeather( d ) {
 };
 
 window.onload = function() {
-    weatherBalloon( 588335 );
+    weatherBalloon( 'Tartu' );
 };
