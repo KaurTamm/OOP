73d964362045cfc8bc6c818c538dd43a2799a69f
KaurTamm <kaur.tamm@khk.ee>
2019-12-09 14:38:58 +0200

    Weather Complete

--- weatherapp.js
+++ weatherapp.js
@@ -3,13 +3,31 @@ function weatherBalloon( cityID ) {
     fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)  
     .then(function(resp) { return resp.json() }) // Convert data to json
     .then(function(data) {
-      console.log(data);
+        console.log(data);
+        drawWeather(data); // Call drawWeather
     })
     .catch(function() {
       // catch any errors
     });
-  }
+};
   
-  window.onload = function() {
+function drawWeather( d ) {
+	var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
+	
+	document.getElementById('description').innerHTML = d.weather[0].description;
+	document.getElementById('temp').innerHTML = celcius + '&deg;';
+    document.getElementById('location').innerHTML = d.name;
+    
+    if( description.indexOf('rain') > 0 ) {
+        document.body.className = 'rainy';
+    } else if( description.indexOf('cloud') > 0 ) {
+        document.body.className = 'cloudy';
+    } else if( description.indexOf('sunny') > 0 ) {
+        document.body.className = 'sunny';
+    }
+};
+
+window.onload = function() {
     weatherBalloon( 588335 );
-  }
+};
