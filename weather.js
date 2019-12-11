672bd6ff232b075a2e380a0647963419f799b552
KaurTamm <kaur.tamm@khk.ee>
2019-12-11 15:09:38 +0200

    Weather data print

---
+++ weather.js
@@ -0,0 +1,15 @@
+class Weather{
+    //  Constructor
+    constructor(c){
+        this.city = c;
+        this.key ='f19190e7226af07a2dfe8b42df98b83a';
+    };
+    //  Data from API
+    async weatherData(){
+        const resp = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + this.city+ '&appid=' + this.key);  
+        const data = await resp.json();
+        console.log(data);
+        return data;
+    };
+    // Change city name
+};
--- weatherapp.js
+++ weatherapp.js
@@ -1,33 +1,12 @@
-function weatherBalloon( cityID ) {
-    var key = 'f19190e7226af07a2dfe8b42df98b83a';
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityID+ '&appid=' + key)  
-    .then(function(resp) { return resp.json() }) // Convert data to json
-    .then(function(data) {
-        console.log(data);
-        drawWeather(data); // Call drawWeather
-    })
-    .catch(function() {
-      // catch any errors
-    });
-};
-  
-function drawWeather( d ) {
-	var celcius = Math.round(parseFloat(d.main.temp)-273.15);
-	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
-	
-	document.getElementById('description').innerHTML = d.weather[0].description;
-	document.getElementById('temp').innerHTML = celcius + '&deg;';
-    document.getElementById('location').innerHTML = d.name;
-    
-    if( description.indexOf('rain') > 0 ) {
-        document.body.className = 'rainy';
-    } else if( description.indexOf('cloud') > 0 ) {
-        document.body.className = 'cloudy';
-    } else if( description.indexOf('sunny') > 0 ) {
-        document.body.className = 'sunny';
-    }
-};
+//  Weather for given location
+const weather = new Weather('Tartu');
 
-window.onload = function() {
-    weatherBalloon( 'Tartu' );
-};
+//  app UI
+const ui = new UI();
+
+weather.weatherData().then(cityWeather => {
+    ui.print(cityWeather);
+}).catch(error => console.log(error));
+
+
+console.log(ui);
