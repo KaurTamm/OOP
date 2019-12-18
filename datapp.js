0143035d36ed931b04a28dc051a9f4eee9f11a14
KaurTamm <kaur.tamm@khk.ee>
2019-12-18 11:10:00 +0200

    Fetch JSON

--- datapp.js
+++ datapp.js
@@ -1,5 +1,7 @@
 document.getElementById('btn1').addEventListener('click', getTextData);
+document.getElementById('btn2').addEventListener('click', getJsonData);
 
+// Local Text
 function getTextData(){
     fetch('text.txt').then(function(res){
         return res.text();
@@ -13,3 +15,22 @@ function getTextData(){
     });
 };
 
+
+// Local JSON
+function getJsonData(){
+    fetch('data.json').then(function(res){
+        return res.json();
+    })
+    .then(function(data){
+        console.log(data);
+        let out = '';
+        data.forEach(function(timetable){
+            out = out + `<li>${timetable.aine} (${timetable.opetaja})</li>`;
+            console.log(out);
+        });
+        document.getElementById('out').innerHTML = out;
+    })
+    .catch(function(error){
+        console.log(error);
+    });
+};
