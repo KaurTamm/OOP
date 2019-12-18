962417dd7d725fcced3d1734862dbc33b39ea00b
KaurTamm <kaur.tamm@khk.ee>
2019-12-18 11:33:25 +0200

    Fetch JSON

--- datapp.js
+++ datapp.js
@@ -1,5 +1,6 @@
 document.getElementById('btn1').addEventListener('click', getTextData);
 document.getElementById('btn2').addEventListener('click', getJsonData);
+document.getElementById('btn3').addEventListener('click', getApiData);
 
 // Local Text
 function getTextData(){
@@ -28,6 +29,25 @@ function getJsonData(){
             out = out + `<li>${timetable.aine} (${timetable.opetaja})</li>`;
             console.log(out);
         });
+        document.getElementById('out').innerHTML = out;
+    })
+    .catch(function(error){
+        console.log(error);
+    });
+};
+
+// Remote API
+function getApiData(){
+    fetch('https://api.github.com/users').then(function(res){
+        return res.json();
+    })
+    .then(function(data){
+        console.log(data);
+        let out = '';
+        data.forEach(function(users){
+            out = out + `<li>${users.login}<br>`
+            out = out + `<img src="${users.avatar_url}" width="100px"></li>`
+        })
         document.getElementById('out').innerHTML = out;
     })
     .catch(function(error){
